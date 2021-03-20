526a54734101aa95ab4de30831d91378964f0323
Andres Felipe Herndandez <38327255+felipedelosh@users.noreply.github.com>
2021-03-19 22:14:24 -0500

    Codigo optimizado

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ class Software:
         self.canvas = Canvas(self.screem, width=1280, height=720, bg="black")
         self.center = [int(self.canvas['width'])/2, int(self.canvas['height'])/2]
         self.prime = [] # Colection of primes
-        self.primes(6666) # Put a primes into array
-        self.scale = self.center[0] / len(self.prime) # distance bwwtwen point to point
+        self.primes(9999) # Put a primes into array
+        self.scale = (self.center[1] / len(self.prime))*0.6 # distance bwwtwen point to point
 
         self.showScreem()
 
@@ -41,32 +41,39 @@ class Software:
         """
         draw xy pont, (r, o) r e prime N o e rad
         """
+        p = x
         # Calculate a R
         r = self.scale * x
+
         x = self.center[0] + (r*math.cos(y))
         y = self.center[1] - (r*math.sin(y))
+
         self.canvas.create_oval(x, y, x+3, y+3, fill="yellow")
+
+        # If you draw less 300 prime numbers put a ponit
+        if len(self.prime) < 80:
+            self.canvas.create_text(x,y-10, fill="yellow", text=str(p))
+
 
     def primes(self, n):
         """
         Return a colection with n primes
         """
-        cont = 1
+        cont = 2
         while len(self.prime) < n:
             if self.isPrime(cont):
                 self.prime.append(cont)
+
             cont = cont + 1
 
-
     def isPrime(self, x):
-        cont = 0
-
-        if x < 0:
+        if x <= 1 or (x>2 and x%2==0) or (x>3 and x%3==0) or (x>5 and x%5==0):
             return False
-        elif x == 1 or x == 2:
+        elif x == 2 or x == 3:
             return True
         else:
-            for i in range(2, x+1):
+            cont = 0
+            for i in range(3, x+1):
                 if x%i == 0:
                     cont = cont + 1
                     if cont > 1:
